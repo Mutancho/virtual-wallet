fbe1bed24365a5a4156a4300ca7eb27811b7db12
AntonNikiforov <nikifor.anto@gmail.com>
2023-05-17 18:42:32 +0300

    Moving send email functionality to utils. Ref #21

--- backend/services/user_service.py
+++ backend/services/user_service.py
@@ -1,10 +1,8 @@
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
 from database.database_queries import read_query,update_query,insert_query
 from schemas.user_models import RegisterUser, EmailLogin, UsernameLogin, DisplayUser
 from utils.passwords import hash_password, verify_password
 from utils import oauth2
+from utils.send_emails import send_email
 
 async def create(user:RegisterUser) -> RegisterUser:
     hashed = await hash_password(user.password)
@@ -123,34 +121,6 @@ async def exists_by_id(token):
 
     return len(data) > 0
 
-async def send_email(recipient_email,confirmation_link = None ,subject=None, message = None ):
-    sender_email = "virtual.wallet.team1@gmail.com"
-    sender_password = "nowqjgrjcgbnhuvu"
-
-    msg = MIMEText(message)
-
-    msg["From"] = sender_email
-    msg["To"] = recipient_email
-    msg["Subject"] = subject
-
-    try:
-        # Create a secure connection with the SMTP server
-
-        server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-        server.ehlo()
-
-        # Login to the sender's email account
-        server.login(sender_email, sender_password)
-
-        # Send the email
-        server.sendmail(sender_email, recipient_email, msg.as_string())
-        print("Confirmation email sent successfully!")
-
-        # Close the connection
-        server.close()
-    except Exception as e:
-        print("Failed to send confirmation email.")
-        print(e)
 
 
 
