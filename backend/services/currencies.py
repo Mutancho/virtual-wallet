353598d7580512d1359369844f2d9c168d440d6d
DimitarP <dpolihronov1@gmail.com>
2023-05-23 15:27:41 +0300

    add get_all_currencies() Ref #53

--- backend/services/currencies.py
+++ backend/services/currencies.py
@@ -4,3 +4,8 @@ from database.database_queries import read_query
 async def select_currency(currency: str):
     get_currency_id = await read_query("SELECT id FROM currencies WHERE currency=%s", (currency.upper(),))
     return get_currency_id
+
+
+async def get_all_currencies():
+    get_currencies = read_query("SELECT * FROM currencies")
+    return get_currencies
