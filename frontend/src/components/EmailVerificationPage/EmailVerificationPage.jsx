a915c4d3bb3c4d63841e922853c226791f28acdb
AntonNikiforov <nikifor.anto@gmail.com>
2023-06-02 15:38:02 +0300

    Added a home button to EmailVerificationPage.

--- frontend/src/components/EmailVerificationPage/EmailVerificationPage.jsx
+++ frontend/src/components/EmailVerificationPage/EmailVerificationPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './EmailVerificationPage.css';
 
 function EmailVerificationPage() {
@@ -6,6 +7,7 @@ function EmailVerificationPage() {
     <div className="email-verification-container">
       <h2>Verify Your Email Address</h2>
       <p>We have sent you an email with a link to verify your account. Please check your inbox and follow the instructions to complete your registration.</p>
+      <Link to="/">Go to Home Page</Link>
     </div>
   );
 }
