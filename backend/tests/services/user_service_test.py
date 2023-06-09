645288b1faea63892ed43fb739157dde0b1dc450
AntonNikiforov <nikifor.anto@gmail.com>
2023-06-09 21:16:45 +0300

    Updated user tests to work with database transactions and added new return data to login. Ref #56

--- backend/tests/services/user_service_test.py
+++ backend/tests/services/user_service_test.py
@@ -1,3 +1,5 @@
+import asyncio
+import json
 import unittest
 from unittest.mock import Mock, patch
 from schemas.user_models import RegisterUser, EmailLogin, UsernameLogin, DisplayUser, UpdateUser, AfterUpdateUser, BlockUnblock, Username
@@ -5,6 +7,9 @@ from services import user_service
 from utils.passwords import hash_password, verify_password
 from utils import oauth2
 from asyncio import run
+from asyncmy.connection import Connection
+import responses
+import imp
 
 USERNAME='DeanWinchester'
 PASSWORD='Deans10?'
@@ -18,24 +23,42 @@ TWO_FACTOR = None
 TITLE = 'Mr'
 GENDER = 'male'
 TOKEN = 'Bearer "'+oauth2.create_access_token(1)+'"'
+CONNECTION = Mock(spec=Connection)
+
 
 
 class UserService_Should(unittest.TestCase):
+    def setUp(self):
+
+        def kill_patches():
+            patch.stopall()
+            imp.reload(user_service)
+
+        self.addCleanup(kill_patches)
+
     @patch('services.user_service.insert_query', autospec=True)
-    def test_create_createsUser(self, mock_insert_query):
+    @patch('services.user_service.update_query', autospec=True)
+    @patch('services.user_service.manage_db_transaction', lambda x: CONNECTION ).start()
+    def test_create_createsUser(self, mock_manage_db_transaction, mock_update_query, mock_insert_query ):
         async def async_test():
             user = RegisterUser(
-                username=USERNAME,password=PASSWORD,first_name=FIRST_NAME,last_name=LAST_NAME,
-            email=EMAIL, phone_number=PHONE_NUMBER, date_of_birth=DATE_OF_BIRTH, address=ADDRESS,identity_document='photo' )
+                    username=USERNAME, password=PASSWORD, first_name=FIRST_NAME, last_name=LAST_NAME,
+                    email=EMAIL, phone_number=PHONE_NUMBER, date_of_birth=DATE_OF_BIRTH, address=ADDRESS,
+                    identity_document='photo'
+                )
             mock_insert_query.return_value = 1
+            mock_manage_db_transaction.return_value = CONNECTION
             result = await user_service.create(user)
 
             self.assertEqual(RegisterUser, type(result))
             self.assertEqual(1, result.id)
+
         run(async_test())
 
     @patch('services.user_service.update_query', autospec=True)
-    def test_confirm(self, mock_update_query):
+    @patch('services.user_service.manage_db_transaction', lambda x: CONNECTION).start()
+    def test_confirm(self,manage_db_transaction, mock_update_query):
+
         async def async_test():
             expected_html = '''
     <!DOCTYPE html>
@@ -64,6 +87,7 @@ class UserService_Should(unittest.TestCase):
     </body>
     </html>
     '''
+            manage_db_transaction.return_value = CONNECTION
             result = await user_service.confirm(1)
 
             self.assertEqual(expected_html,result)
@@ -71,38 +95,49 @@ class UserService_Should(unittest.TestCase):
         run(async_test())
 
     @patch('services.user_service.insert_query', autospec=True)
-    def test_login_EmailLogin(self, mock_insert_query):
+    @patch('services.user_service.manage_db_transaction', lambda x: CONNECTION).start()
+    def test_login_EmailLogin(self,manage_db_transaction, mock_insert_query):
+        @responses.activate
         async def async_test():
             with patch('services.user_service.read_query') as mock_read_query:
                 credentials = EmailLogin(email=EMAIL, password=PASSWORD)
                 mock_read_query.return_value = [(1,EMAIL,0)]
                 token = oauth2.create_access_token(1)
+                responses.get(url='https://api.chatengine.io/users/me/', status=200,
+                              json={'username': EMAIL, 'secret': PASSWORD})
+                response = {'username': EMAIL, 'secret': PASSWORD}
 
                 result_emailLogin = await user_service.login(credentials)
 
                 self.assertEqual(EmailLogin,type(credentials))
-                self.assertEqual(dict(access_token=token, token_type="bearer",is_admin=False),result_emailLogin)
+                self.assertEqual(dict(access_token=token, token_type="bearer",is_admin=False,response_data= response),result_emailLogin)
 
 
         run(async_test())
 
     @patch('services.user_service.insert_query', autospec=True)
-    def test_login_UsernameLogin(self, mock_insert_query):
+    @patch('services.user_service.manage_db_transaction', lambda x: CONNECTION).start()
+    def test_login_UsernameLogin(self,manage_db_transaction, mock_insert_query):
+        @responses.activate
         async def async_test():
             with patch('services.user_service.read_query') as mock_read_query:
                 credentials = UsernameLogin(username=USERNAME,password=PASSWORD)
                 mock_read_query.return_value = [(1, USERNAME,1)]
                 token = oauth2.create_access_token(1)
 
+                responses.get(url = 'https://api.chatengine.io/users/me/',status = 200,json={'username':USERNAME,'secret': PASSWORD})
+                response = {'username':USERNAME,'secret': PASSWORD}
+
                 result_usernameLogin = await user_service.login(credentials)
 
                 self.assertEqual(UsernameLogin, type(credentials))
-                self.assertEqual(dict(access_token=token, token_type="bearer",is_admin=True), result_usernameLogin)
+                self.assertEqual(dict(access_token=token, token_type="bearer",is_admin=True,response_data= response), result_usernameLogin)
 
         run(async_test())
 
     @patch('services.user_service.update_query', autospec=True)
-    def test_logout(self, mock_update_query):
+    @patch('services.user_service.manage_db_transaction', lambda x: CONNECTION).start()
+    def test_logout(self,manage_db_transaction, mock_update_query):
         async def async_test():
             token = TOKEN
             result = await user_service.logout(token)
@@ -112,7 +147,8 @@ class UserService_Should(unittest.TestCase):
         run(async_test())
 
     @patch('services.user_service.read_query', autospec=True)
-    def test_all(self, mock_read_query):
+    @patch('services.user_service.manage_db_transaction', lambda x: CONNECTION).start()
+    def test_all(self,manage_db_transaction, mock_read_query):
         async def async_test():
             user = DisplayUser(username=USERNAME, email=EMAIL, phone_number=PHONE_NUMBER, first_name=FIRST_NAME, last_name=LAST_NAME, address=ADDRESS)
             mock_read_query.return_value = [(USERNAME,EMAIL,PHONE_NUMBER,FIRST_NAME,LAST_NAME,ADDRESS)]
@@ -124,7 +160,8 @@ class UserService_Should(unittest.TestCase):
         run(async_test())
 
     @patch('services.user_service.update_query', autospec=True)
-    def test_delete(self, mock_update_query):
+    @patch('services.user_service.manage_db_transaction', lambda x: CONNECTION).start()
+    def test_delete(self,manage_db_transaction, mock_update_query):
         async def async_test():
             with patch('services.user_service.read_query') as mock_read_query:
                 user = DisplayUser(username=USERNAME, email=EMAIL, phone_number=PHONE_NUMBER, first_name=FIRST_NAME,
@@ -138,7 +175,8 @@ class UserService_Should(unittest.TestCase):
         run(async_test())
 
     @patch('services.user_service.update_query', autospec=True)
-    def test_update(self, mock_update_query):
+    @patch('services.user_service.manage_db_transaction', lambda x: CONNECTION).start()
+    def test_update(self,manage_db_transaction, mock_update_query):
         async def async_test():
             with patch('services.user_service.read_query') as mock_read_query:
                 user = UpdateUser(new_password='Newpass10!',repeat_password='Newpass10!',old_password=PASSWORD,email=EMAIL,
@@ -156,7 +194,8 @@ class UserService_Should(unittest.TestCase):
         run(async_test())
 
     @patch('services.user_service.update_query', autospec=True)
-    def test_block_unblock_Blocks(self, mock_update_query):
+    @patch('services.user_service.manage_db_transaction', lambda x: CONNECTION).start()
+    def test_block_unblock_Blocks(self,manage_db_transaction, mock_update_query):
         async def async_test():
             command = BlockUnblock(action='block')
             result = await user_service.block_unblock(1,command)
@@ -166,7 +205,8 @@ class UserService_Should(unittest.TestCase):
         run(async_test())
 
     @patch('services.user_service.update_query', autospec=True)
-    def test_block_unblock_Unblocks(self, mock_update_query):
+    @patch('services.user_service.manage_db_transaction', lambda x: CONNECTION).start()
+    def test_block_unblock_Unblocks(self,manage_db_transaction, mock_update_query):
         async def async_test():
             command = BlockUnblock(action='unblock')
             result = await user_service.block_unblock(1, command)
@@ -176,7 +216,8 @@ class UserService_Should(unittest.TestCase):
         run(async_test())
 
     @patch('services.user_service.read_query', autospec=True)
-    def test_getUser_returnsUsername(self, mock_read_query):
+    @patch('services.user_service.manage_db_transaction', lambda x: CONNECTION).start()
+    def test_getUser_returnsUsername(self,manage_db_transaction, mock_read_query):
         async def async_test():
             user = Username(username=USERNAME)
             mock_read_query.return_value = [(USERNAME,)]
@@ -188,7 +229,8 @@ class UserService_Should(unittest.TestCase):
         run(async_test())
 
     @patch('services.user_service.read_query', autospec=True)
-    def test_getUser_returnsNotFoundMessage(self, mock_read_query):
+    @patch('services.user_service.manage_db_transaction', lambda x: CONNECTION).start()
+    def test_getUser_returnsNotFoundMessage(self,manage_db_transaction, mock_read_query):
         async def async_test():
 
             mock_read_query.return_value = []
@@ -198,3 +240,4 @@ class UserService_Should(unittest.TestCase):
             self.assertEqual("User not found", result)
 
         run(async_test())
+
