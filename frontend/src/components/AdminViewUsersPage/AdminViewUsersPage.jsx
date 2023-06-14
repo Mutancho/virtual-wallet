fac904e5f520a427df8817dada4faa5041cc85dd
AntonNikiforov <nikifor.anto@gmail.com>
2023-06-14 10:21:07 +0300

    Changed styling. Ref #78,#89

--- frontend/src/components/AdminViewUsersPage/AdminViewUsersPage.jsx
+++ frontend/src/components/AdminViewUsersPage/AdminViewUsersPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Sidebar from "../SideBar/SideBar";
+import './AdminViewUsersPage.css'
 
 function AdminViewUsersPage() {
   const [users, setUsers] = useState([]);
