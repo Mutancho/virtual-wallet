f49be3bc300e4ade5808b88acd6625535f752065
DimitarP <dpolihronov1@gmail.com>
2023-06-14 14:53:52 +0300

    fix styling Ref #89, #70, #78, #79

--- frontend/src/components/AdminViewUsersPage/AdminViewUsersPage.jsx
+++ frontend/src/components/AdminViewUsersPage/AdminViewUsersPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Sidebar from "../SideBar/SideBar";
+import Sidebar from '../SideBar/SideBar';
+import './AdminViewUsersPage.css';
 
 function AdminViewUsersPage() {
   const [users, setUsers] = useState([]);
@@ -60,75 +61,84 @@ function AdminViewUsersPage() {
   };
 
   return (
-    <div className="admin-view-users">
-      <Sidebar/>
-      <h2>Admin View Users</h2>
-      <div className="search-bar">
-        <input
-          type="text"
-          value={usernameSearchTerm}
-          onChange={handleUsernameSearchTermChange}
-          placeholder="Search by username"
-        />
-        <input
-          type="text"
-          value={emailSearchTerm}
-          onChange={handleEmailSearchTermChange}
-          placeholder="Search by email"
-        />
-        <input
-          type="text"
-          value={phoneSearchTerm}
-          onChange={handlePhoneSearchTermChange}
-          placeholder="Search by phone number"
-        />
-        <button onClick={handleSearch}>Search</button>
-      </div>
-      <div className="pagination-controls">
-        <label>
-          Limit:
+    <div className="admin-view-users-page">
+      <Sidebar />
+      <div className="admin-view-users-content">
+        <h2>Admin View Users</h2>
+        <div className="search-bar">
           <input
-            type="number"
-            value={limit}
-            onChange={handleLimitChange}
-            min={0}
+            type="text"
+            id="username"
+            value={usernameSearchTerm}
+            onChange={handleUsernameSearchTermChange}
+            placeholder="Search by username"
           />
-        </label>
-        <label>
-          Offset:
           <input
-            type="number"
-            value={offset}
-            onChange={handleOffsetChange}
-            min={0}
+            type="text"
+            id="email"
+            value={emailSearchTerm}
+            onChange={handleEmailSearchTermChange}
+            placeholder="Search by email"
           />
-        </label>
-      </div>
-      <div className="user-list-wrapper">
-        <ul className="user-list">
-          {users.map((user) => (
-            <li key={user.id}>
-              <div>
-                <strong>Username:</strong> {user.username}
-              </div>
-              <div>
-                <strong>Email:</strong> {user.email}
-              </div>
-              <div>
-                <strong>Phone Number:</strong> {user.phone_number}
-              </div>
-              <div>
-                <strong>First Name:</strong> {user.first_name}
-              </div>
-              <div>
-                <strong>Last Name:</strong> {user.last_name}
-              </div>
-              <div>
-                <strong>Address:</strong> {user.address}
-              </div>
-            </li>
-          ))}
-        </ul>
+          <input
+            type="text"
+            id="phone"
+            value={phoneSearchTerm}
+            onChange={handlePhoneSearchTermChange}
+            placeholder="Search by phone number"
+          />
+          <button className="submit-button" onClick={handleSearch}>
+            Search
+          </button>
+        </div>
+        <div className="pagination-controls">
+          <label htmlFor="limit">
+            Limit:
+            <input
+              type="number"
+              id="limit"
+              value={limit}
+              onChange={handleLimitChange}
+              min={0}
+            />
+          </label>
+          <label htmlFor="offset">
+            Offset:
+            <input
+              type="number"
+              id="offset"
+              value={offset}
+              onChange={handleOffsetChange}
+              min={0}
+            />
+          </label>
+        </div>
+        <div className="user-list-wrapper">
+          <ul className="user-list">
+            {users.map((user) => (
+              <li key={user.id}>
+                <div>
+                  <strong>Username:</strong> {user.username}
+                </div>
+                <div>
+                  <strong>Email:</strong> {user.email}
+                </div>
+                <div>
+                  <strong>Phone Number:</strong> {user.phone_number}
+                </div>
+                <div>
+                  <strong>First Name:</strong> {user.first_name}
+                </div>
+                <div>
+                  <strong>Last Name:</strong> {user.last_name}
+                </div>
+                <div>
+                  <strong>Address:</strong> {user.address}
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
